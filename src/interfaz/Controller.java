1a023043723c40dbe3d261dc57d9a25088d17a52
Jhon Ospina <jhonjames0510@gmail.com>
2019-07-06 18:12:22 -0500

    Interfaz con table <3
    Ya aprendí a hacer tablas wiii

--- src/interfaz/Controller.java
+++ src/interfaz/Controller.java
@@ -1,16 +1,40 @@
 package interfaz;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import logica.AnalizadorLexico;
 import logica.Token;
 
 public class Controller  {
 
-	
+
+    @FXML
+    private TableView<TokensView> tableTokens;
+
+    @FXML
+    private TableColumn<TokensView, String> colPalabra;
+
+    @FXML
+    private TableColumn<TokensView, String> colCategoria;
+
+    @FXML
+    private TableColumn<TokensView, String> colFila;
+
+    @FXML
+    private TableColumn<TokensView, String> colColumna;
+
+    @FXML
+    private TableColumn<TokensView, String> colAutomata;
+    
+    ObservableList<TokensView> tokens;
 
 	@FXML
 	private TextArea txtArea;
@@ -61,6 +85,30 @@ public class Controller  {
 	private MenuItem menuComentarios;
 
 	
+
+    @FXML
+    void initialize() {
+    
+
+    	tokens = FXCollections.observableArrayList();
+    	colPalabra.setCellValueFactory(
+    			 new PropertyValueFactory<TokensView,String>("palabra")
+    			);
+    			colCategoria.setCellValueFactory(
+    			 new PropertyValueFactory<TokensView,String>("categoria")
+    			);
+    			colFila.setCellValueFactory(
+    			 new PropertyValueFactory<TokensView,String>("fila")
+    			);
+    			colColumna.setCellValueFactory(
+    			 new PropertyValueFactory<TokensView,String>("columna")
+    			);
+    			colAutomata.setCellValueFactory(
+    			 new PropertyValueFactory<TokensView,String>("automata")
+    			);
+    	
+    }
+	
 	@FXML
 	void onButtonAnalizar(ActionEvent event) {
 
@@ -78,79 +126,7 @@ public class Controller  {
 
 	}
 
-	@FXML
-	void onButtonCadena(ActionEvent event) {
 
-	}
 
-	@FXML
-	void onButtonComentarios(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonHexa(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonIdentificadores(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonLlaves(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonNoNaturales(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonNoReales(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonOpAisgnacion(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonOpAritmeticos(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonOpIncremento(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonOpLogicos(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonOpRelacionales(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonParentesis(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonSeparador(ActionEvent event) {
-
-	}
-
-	@FXML
-	void onButtonTerminal(ActionEvent event) {
-
-	}
 
 }
